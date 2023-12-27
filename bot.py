de73bd710b9d4d10532ae5d8179ef5becde77a6a
Stretyws <dsgfh@gmail.com>
2023-12-27 21:24:34 +0300

    init

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from aiogram import Dispatcher, Bot, F
 from aiogram.filters import Command
 from aiogram.types import Message
 from envparse import Env
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 
 env = Env()
 
@@ -12,14 +14,50 @@ TOKEN = env.str('TOKEN')
 dp = Dispatcher()
 
 
+class Profile(StatesGroup):
+    name = State()
+    age = State()
+    color = State()
+    group = State()
+
 @dp.message(Command("start"))
-async def start(message: Message) -> None:
-    username = message.from_user.username
-    full_name = message.from_user.full_name
-    await message.answer(
-        text=f"Привет, {full_name}!\n"
-             f"Твой username - {username}"
-    )
+async def start(message: Message, state: FSMContext):
+    await message.answer("Привет!")
+    await message.answer("Представься пожалуйста!")
+    await state.set_state(Profile.name)
+
+@dp.message(Profile.name)
+async def name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer("Хорошо, сколько тебе лет?")
+    await state.set_state(Profile.age)
+
+@dp.message(Profile.age)
+async def name(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer("Хорошо, какой твой любимый цвет?")
+    await state.set_state(Profile.color)
+
+@dp.message(Profile.color)
+async def name(message: Message, state: FSMContext):
+    await state.update_data(color=message.text)
+    await message.answer("Хорошо, в какой ты группе?")
+    await state.set_state(Profile.group)
+
+# @dp.message(Command("start"))
+# async def start(message: Message) -> None:
+#     username = message.from_user.username
+#     full_name = message.from_user.full_name
+#     await message.answer(
+#         text=f"Привет, {full_name}!\n"
+#              f"Твой username - {username}"
+#     )
+# @dp.message(F.text.lower().contains("кошка"))
+# async def cat(message: Message) -> None:
+#     await message.answer(text="Здесь есть кошка")
+# @dp.message()
+# async def dog(message: Message) -> None:
+#     await message.answer(text="Здесь есть собака")
 
 
 async def main() -> None:
